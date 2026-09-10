d4cf615f8efd0123115053958520f061e1577e94
TNQ22 <quangthanhngo22@gmail.com>
2026-09-10 23:54:48 +0700

    fix(ci): remove unused maps_launcher and force compileSdkVersion 35 for all plugins

--- macless_haystack/lib/accessory/accessory_list.dart
+++ macless_haystack/lib/accessory/accessory_list.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:maps_launcher/maps_launcher.dart';
 import 'package:provider/provider.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:macless_haystack/dashboard/app_toast.dart';
